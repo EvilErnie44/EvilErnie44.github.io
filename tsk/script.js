0c33b542403a143ccd1b70735b1b7c7434ea5dec
Simon Dean <simondeanmbro@gmail.com>
2018-07-20 15:12:49 +0100

    tsk push

---
+++ tsk/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+$('.menu').on("click", function(){
+  $('.navul').toggleClass('active'); 
+    })
+})
+
+$(document).ready(function(){
+$('.inl1').on("click", function(){ 
+    $('.list1').toggleClass('showitem'); 
+    })
+})
+
+$(document).ready(function(){
+$('.inl2').on("click", function(){ 
+    $('.list2').toggleClass('showitem'); 
+    })
+})
+
+$(document).ready(function(){
+$('.inl3').on("click", function(){ 
+    $('.list3').toggleClass('showitem'); 
+    })
+})
