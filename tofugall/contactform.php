f6c8864eeab1a259882b204e1a4e0362bcf3dd4b
Simon Dean <simondeanmbro@gmail.com>
2018-07-26 15:56:25 +0100

    email

---
+++ tofugall/contactform.php
@@ -0,0 +1,17 @@
+<?php 
+
+if (isset($_POST['submit'])){
+    
+    $name = $_POST['name']; 
+    $subject = $_POST['subject']; 
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+    
+    $mailTo = "simondean888@hotmail.co.uk"
+    $headers = "From: ".$mailFrom;
+    $text = "You have recieved an e-mail from ".$name.".\n\n".$message; 
+        
+    mail($mailTo,$subject,$text,$headers);
+    
+    header("Location: index.php?mailsend");    
+}
